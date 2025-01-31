d13b05eaef9e20be29834278596f735ace1f88fa
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-31 22:27:57 +0530

    modified

--- foodDelivery.js
+++ foodDelivery.js
@@ -7,6 +7,7 @@ const display = (time, msg, order = "") => {
 };
 
 const deliveringOrder = (order, startTime) => {
+    display(timeStamping(startTime), "Delivery order....");
   setTimeout(() => {
     const time = timeStamping(startTime);
     const msg = "Order delivered: ";
@@ -16,37 +17,37 @@ const deliveringOrder = (order, startTime) => {
 };
 
 const packingOrder = (order, startTime) => {
+    display(timeStamping(startTime), "Packing order...");
+
   setTimeout(() => {
     const time = timeStamping(startTime);
     const msg = "order packed :";
     order["packageDetails"] = "Packed in eco-friendly box";
     display(time, msg, order);
-    display(timeStamping(startTime), "Delivery order....");
     deliveringOrder(order, startTime);
   }, 2000);
 };
 
 const preparingFood = (order, startTime) => {
   display(timeStamping(startTime), "Preparing food...");
-
   setTimeout(() => {
     const time = timeStamping(startTime);
     const msg = "Food is ready : ";
     order["foodDetails"] = "Burger & Fries";
     display(time, msg, order);
-    display(timeStamping(startTime), "Packing order...");
     packingOrder(order, startTime);
   }, 3000);
 };
 
-const orderRecieved = function (startTime) {
+const orderRecieved = function (order, startTime) {
   display(timeStamping(startTime), "Order received: ", { orderId: 123 });
-  preparingFood({ orderId: 123 }, startTime);
+  preparingFood(order, startTime);
 };
 
 const main = () => {
   const startTime = Date.now();
-  orderRecieved(startTime);
+  const order = { orderId: 123 };
+  orderRecieved(order, startTime);
 };
 
 main();
